b1ea6343e9869e76572ec858957ae75142107865
WhenRaptorsAttack <pykedout@gmail.com>
2011-03-19 22:42:33 -0700

    forgot index.js

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
 var connect = require('connect'),
     dnode = require('dnode'),
-    mongoose = require('mongoose'),
     config = require('./config'),
-    models = require('./models'),
-    Schema = mongoose.Schema;
+    iface = require('./interface');
     
-var db = mongoose.connect(config.db);
-mongoose.model('User', new Schema(models.User));
-var User = mongoose.model('User');
+    
 var server = connect.createServer();
 server.use(connect.static(__dirname+'/static'));
 
 dnode(function(client) {
     this.ready = function(ready) {
-        var interface = {
-            login: function(params, callback) {
-                
-            },
-            create: function(params, success, fail) {
-                var errs = [];
-                console.log(User.find({name:params.name}));
-                User.find({name:params.name}, function(err, docs) {
-                    if(docs.length > 0) {
-                        errs.push({name:'name', message:'Your name was taken :('});
-                        fail(errs);
-                    } else {
-                        User.find({email:params.email}, function(err, docs){
-                            if(docs.length > 0) {
-                                errs.push({name:'email', message:'Hey! That email is already registered!'});
-                                fail(errs);
-                            } else {
-                                new User(params).save();
-                                success();
-                            }
-                        });
-                    }
-                });
-            }
-        };
-        ready(interface);
+        ready(iface.IFaceLogin);
     };
-    
 }).listen(server);
 server.listen(404);
+
+
